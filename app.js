c6d0d7d104d23254b3520b8f455b6d80be7a3437
Alejandro Gutiérrez <alexgtz.up@gmail.com>
2022-05-02 21:57:59 -0500

    Añadiendo script para crear nueva card

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
 let cardTitle = `Card Nueva ${new Date()}`
 
-trello.addCard(cardTitle, "LaunchX Card Description", "6264e42be72d295e64f5c083",
+trello.addCard(cardTitle, "LaunchX Card Description", "6270890010064220ca9fb923",
 	function (error, trelloCard) {
 		if (error) {
 			console.log('Could not add card:', error);
